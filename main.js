e142313f12ab456de59af56a89291dd736f979a3
gerov18 <81087572+gerov18@users.noreply.github.com>
2022-05-22 16:05:43 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+function bestSprintSequence(sprints) {
+
+    let count = 0;
+    let sequence;
+    let sequencesList = [];
+    let sumList;
+
+    if (sprints.length < 3) {
+        console.log("Error!");
+    } else {
+        for (let i = 0; i < sprints.length; i++) {
+            sequence = sprints.slice(i, i + 3);
+            if (sequence.length === 3) {
+                count += 1;
+                sequencesList.push(sequence);
+            }
+        }
+        sumList = sequencesList.map(r => r.reduce((a, b) => a + b));
+
+        let maxNum = Math.max(...sumList);
+        let indexList = [];
+
+        for (let i = 0; i < sumList.length; i++) {
+            if (sumList[i] === maxNum) {
+                indexList.push(i);
+            }
+        }
+        let bestIndex = indexList[indexList.length - 1];
+        console.log("sequence: ", sequencesList[bestIndex], "sum: ", sumList[bestIndex]);
+    }
+}
+let sprints = [];
+let sprint;
+let count = parseInt(prompt("Enter the total number of sprints"));
+
+for (let i = 1; i <= count; i++) {
+    sprint = prompt("Enter completed story points: ").toLowerCase();
+    sprints.push(parseInt(sprint));
+}
+
+bestSprintSequence(sprints);
